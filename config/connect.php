5fc48e33baea70855db376c4fdc539a0ce712834
Daria <you@example.com>
2022-10-05 12:08:40 +0300

    add data for sql

--- config/connect.php
+++ config/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', 'root');
-define('DATABASE', 'log');
+define('USER', 'u1801529_root');
+define('PASSWORD', 'root123456');
+define('DATABASE', 'u1801529_log');
 
 $connect = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
 
